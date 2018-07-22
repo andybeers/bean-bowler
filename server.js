46ad54ceb604c3a0f18bc9eee52e920a0dc83770
Andrew Beers <andybeers@gmail.com>
2018-07-22 08:57:42 -0700

    Oh we scrapin out here

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const axios = require('axios')
 const cheerio = require('cheerio')
 
 const port = process.env.PORT || 3000
 const app = express()
 
-app.get('/bilbos', (req, res) => {
-  res.send('dope dude')
-})
+// Serve up our dope-ass website
 app.use(express.static('./'))
+
+// Scrape dat DOM
+app.get('/api/menu', (req, res, next) => {
+  axios
+    .get('https://cambia.cafebonappetit.com/')
+    .then(response => {
+      const $ = cheerio.load(response.data, {
+        normalizeWhitespace: true,
+        decodeEntities: true,
+      })
+      const menu = $('#cafe-hours-2').text()
+      res.send({ menu })
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+
+// Error Handling
+app.use((err, req, res) => {
+  const code = err.code || 500
+  const error = code === 500 ? 'Internal server error' : err.error
+  console.error(err.error || err.message)
+  res.status(code).send({ error })
+})
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
